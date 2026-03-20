86dcb875eac3f6eca3fe7c3988e7c431ae6a5c84
Diw <tanakitsiriteerapan@gmail.com>
2026-03-20 14:08:21 +0700

    Delete outdated project proposal document for ML/DL Final Project on AI motion boxing controller.

--- _dump_nb.py
+++
@@ -1,7 +0,0 @@
-import json
-
-nb = json.load(open('training/train_temporal.ipynb', encoding='utf-8'))
-for i, c in enumerate(nb['cells']):
-    print(f"=== CELL {i} [{c['cell_type']}] ===")
-    print(''.join(c['source']))
-    print()
